3707781bae7285fb8f2ea8c01ea19234b3e1d3ff
Sean Kearns <c00194323@itcarlow.ie>
2017-09-18 13:24:17 +0100

    Abstract And Bridge Pattern

---
+++ DesignPatterns/DesignPatterns/Bridge.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class Character
+{
+public:
+	Character(){}
+	void Print() { cout << "Printing Character" << endl; }
+};
+class HandleToCharacter
+{
+public:
+	HandleToCharacter() : character(new Character()){}
+	Character* operator->() { return character; }
+private:
+	Character* character;
+
+};
+int main()
+{
+	HandleToCharacter handle;
+	handle->Print();
+	system("PAUSE");
+}
