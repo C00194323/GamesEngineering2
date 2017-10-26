aaaae27a9e42c093fd4ad5286fe94388fc817727
Sean Kearns <c00194323@gmail.com>
2017-10-26 10:56:52 +0100

    ESC Started

--- Entity Component/Source.cpp
+++ Entity Component/Source.cpp
@@ -1,5 +1,8 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include "Entity.h"
+#include "HealthComponent.h"
+#include "AI_System.h"
 #include <iostream>
 
 using namespace std;
@@ -11,6 +14,14 @@ int main(int argc, char* argv[])
 	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
 	SDL_Window* window = SDL_CreateWindow("Hello Sean", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, SDL_WINDOW_SHOWN);
 	SDL_Texture *image = nullptr;
+
+
+	Entity player;
+	HealthComponent hc;
+	player.addComponent(hc);
+
+	AI_System hs;
+	hs.addEntity(player);
 
 	bool isRunning = true;
 
@@ -25,6 +36,7 @@ int main(int argc, char* argv[])
 				isRunning = false;
 			}
 		}
+		hs.update();
 	}
 	SDL_Quit();
 	return 0;
